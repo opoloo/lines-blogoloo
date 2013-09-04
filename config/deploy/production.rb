46f424c88641a94535b5e5fb28becc0761b6e326
Jochen Greif <jochen@opoloo.de>
2013-09-04 14:34:52 +0200

    removed obsolete files

--- config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-set :applicationdir, "/var/rails/blogoloo"
-set :domain, 'blog.opoloo.de'
-
-after "deploy", "refresh_sitemaps"
-task :refresh_sitemaps do
-  run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec rake sitemap:refresh"
-end
--- config/deploy/staging.rb
+++
@@ -1,4 +0,0 @@
-set :applicationdir, "/path/to/rails/staging/lines"
-set :domain, 'staging.lines.com'
-set :git_shallow_clone, false
-set :deploy_via, :copy
