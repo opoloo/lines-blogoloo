138005e9cad788b9b1e701bf8e78eba824ff4599
Max Boll <max@opoloo.de>
2013-09-11 16:34:27 +0200

    Changed the default database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 Author.create([
-  { :name => "Opoloo Squirrel", :email => "squirrel@opoloo.de", :created_at => "2012-12-12 12:12:12", :updated_at => "2012-12-12 12:12:12", :description => "Squirrels are sooooo cute", :gplus_profile => "https://plus.google.com/squirrel" }
+  { :name => "Opoloo Lines", :email => "info@opoloo.com", :created_at => "2012-12-12 12:12:12", :updated_at => "2012-12-12 12:12:12", :description => "Lines is a project by Opoloo.", :gplus_profile => "https://plus.google.com/#" }
 ], :without_protection => true )
 
 User.create([
